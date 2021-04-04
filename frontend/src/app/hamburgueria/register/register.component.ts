56949fce76e3ffa28d843b65cfe7d35c350beb52
s4nderx <phellipe.sander1@gmail.com>
2021-04-04 10:41:42 -0300

    show errors from backend, validate name field required.

--- frontend/src/app/hamburgueria/register/register.component.ts
+++ frontend/src/app/hamburgueria/register/register.component.ts
@@ -12,11 +12,12 @@ import { HamburguerService } from '../services/hamburguer.service';
 export class RegisterComponent implements OnInit {
 
   ingredients: any[] = [];
+  newHamburguer: NewHamburguerModel = new NewHamburguerModel();
+  errors: any[] = [];
   constructor(private service: HamburguerService,
     private toastr: ToastrService,
     private router: Router,) { }
 
-  newHamburguer: NewHamburguerModel = new NewHamburguerModel();
 
   ngOnInit(): void {
     this.getIngredients();
@@ -25,7 +26,6 @@ export class RegisterComponent implements OnInit {
   public getIngredients() : any {
     this.service.findIngredients().subscribe(data => {
       this.ingredients = data.map(x => {return {quantity: 0, name: x.name, price: x.price, id: x.id}});
-      console.log(this.ingredients)
     });
   }
 
@@ -34,9 +34,14 @@ export class RegisterComponent implements OnInit {
     .map(x => { return {id: x.id, quantity: x.quantity}})
     .filter(x => x.quantity !== 0);
 
-    this.service.createBurguer(this.newHamburguer).subscribe(data => {
+    this.service.createBurguer(this.newHamburguer).subscribe(
+      data => {
       this.sucessInsert(data);
-    })
+      },
+      failure => {
+        this.failureInsert(failure);
+      }
+    )
   }
 
   sucessInsert(res: any){
@@ -45,6 +50,13 @@ export class RegisterComponent implements OnInit {
     this.toastr.success('Hamburguer cadastrado com sucesso!', 'Sucesso!');
     this.router.navigate(['/hamburgueria/list']);
   }
+
+  failureInsert(failure: any) {
+
+    this.errors = failure.error?.errors || [];
+    console.log(this.errors );
+    this.toastr.error('Ocorreu um erro!', 'Opa :(');
+  };
 
   inputnumber = 0;
 
@@ -56,6 +68,17 @@ export class RegisterComponent implements OnInit {
     if(item.quantity != 0){
       item.quantity = item.quantity-1;
     }
+
+  }
+
+  verifyValidAndTouched(field: any, form: any) {
+    return (!field.valid && field.touched) || (form.submitted && !field.valid);
+  }
+
+  applyCssError(field: any, form: any) {
+    return {
+        "is-invalid": this.verifyValidAndTouched(field, form),
+    };
 
   }
 }
